a189c0e5474b4b4eb35afddda007ced2bff8939e
LiquidE01 <emma@fastcode.se>
2025-08-16 20:57:02 +0200

    Minor adjustments

--- FastMusicMobile/Controls/IconButtonSmall.xaml.cs
+++ FastMusicMobile/Controls/IconButtonSmall.xaml.cs
@@ -5,13 +5,21 @@ namespace FastMusicMobile.Controls;
 public partial class IconButtonSmall : ContentView
 {
 	public static readonly BindableProperty IconProperty = BindableProperty.Create(nameof(Icon), typeof(string), typeof(IconButtonSmall), string.Empty);
+	public static readonly BindableProperty FontSizeProperty = BindableProperty.Create(nameof(FontSize), typeof(float), typeof(IconButtonSmall), 24.0f);
+	public static readonly BindableProperty CommandProperty = BindableProperty.Create(nameof(Command), typeof(ICommand), typeof(IconButtonSmall), null);
+	
 	public string Icon
 	{
 		get => (string)GetValue(IconProperty);
 		set => SetValue(IconProperty, value);
 	}
 
-	public static readonly BindableProperty CommandProperty = BindableProperty.Create(nameof(Command), typeof(ICommand), typeof(IconButtonSmall), null);
+	public float FontSize
+	{
+		get => (float)GetValue(FontSizeProperty);
+		set => SetValue(FontSizeProperty, value);
+	}
+
 	public ICommand Command
 	{
 		get => (ICommand)GetValue(CommandProperty);
